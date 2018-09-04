cfbedaa156e7837b49750db86c082f66a372697a
Hyper Mediocrity <michael.treyvaud@gmail.com>
2018-09-04 22:21:48 +0100

    yup

--- src/service/controllers/FridgeController.js
+++ src/service/controllers/FridgeController.js
@@ -14,13 +14,22 @@ class FridgeController {
     this.list = this.list.bind(this);
   }
 
+  //  TODO - validation
   async describe(req, res, next) {
-    const { Logger, Validator, DocumentClient } = this;
+    const {
+      Logger, Validator, DocumentClient, TableName,
+    } = this;
     const { body } = req;
     Logger.info('describe');
     try {
       Validator.validateDescribeRequest(body);
-      return res.status(200).json({ hello: 'world' });
+      const { id } = body;
+      const dbParams = {
+        TableName,
+        Key: { id },
+      };
+      const response = await DocumentClient.get(dbParams).promise();
+      return res.status(200).json(response.Item || {});
     } catch (_err) {
       return next(_err);
     }
@@ -47,8 +56,8 @@ class FridgeController {
         Item,
         TableName,
       };
-      const result = await DocumentClient.put(params).promise();
-      return res.status(200).json(result);
+      await DocumentClient.put(params).promise();
+      return res.status(200).json(Item);
     } catch (_err) {
       return next(_err);
     }
@@ -66,25 +75,65 @@ class FridgeController {
     }
   }
 
+  //  TODO: Validation
   async delete(req, res, next) {
-    const { Logger, Validator, DocumentClient } = this;
+    const {
+      Logger, Validator, DocumentClient, TableName,
+    } = this;
     const { body } = req;
     Logger.info('delete');
     try {
       Validator.validateDeleteRequest(body);
-      return res.status(200).json({ hello: 'world' });
+      const { id } = body;
+      const dbParams = {
+        TableName,
+        Key: { id },
+      };
+      await DocumentClient.delete(dbParams).promise();
+      return res.status(200).json({});
     } catch (_err) {
       return next(_err);
     }
   }
 
+  //  TODO: Validation will need to update to query at some point
   async list(req, res, next) {
-    const { Logger, Validator, DocumentClient } = this;
+    const {
+      Logger, Validator, DocumentClient, TableName,
+    } = this;
     const { body } = req;
     Logger.info('list');
     try {
       Validator.validateListRequest(body);
-      return res.status(200).json({ hello: 'world' });
+      const { from, limit } = body;
+      const dbParams = {
+        TableName,
+        ExclusiveStartKey: from,
+        Limit: limit || 50,
+      };
+      const scan = async (_params, _options) => {
+        Logger.info('Query');
+        return DocumentClient.scan(_params).promise().then((_data) => {
+          if (_options.paginated && _data.LastEvaluatedKey) {
+            const updateParams = {
+              ..._params,
+              ExclusiveStartKey: _data.LastEvaluatedKey,
+            };
+            const newData = _data;
+            return scan(updateParams, _options)
+              .then((nextPage) => {
+                newData.Items = newData.Items.concat(nextPage.Items);
+                newData.LastEvaluatedKey = nextPage.LastEvaluatedKey;
+                newData.Count = nextPage.Count;
+                newData.ScannedCount = nextPage.ScannedCount;
+                return newData;
+              });
+          }
+          return _data;
+        });
+      };
+      const result = await scan(dbParams, body);
+      return res.status(200).json(result);
     } catch (_err) {
       return next(_err);
     }
