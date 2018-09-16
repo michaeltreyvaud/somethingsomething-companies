e87158a16ad68c80913b91a483b70cd7e8866124
daniel treyvaud <daniel.treyvaud@gmail.com>
2018-09-16 15:52:27 +0100

    Validation Done

--- src/service/validation/schemas.js
+++ src/service/validation/schemas.js
@@ -65,6 +65,77 @@ const company = {
   LOGO: Joi.string().uri().required().error(new Error('Invalid logo supplied')),
 };
 
+const fooditem = {
+  NAME: Joi.string().required().error(new Error('Invalid name supplied')),
+  BATCH_NUMBER: Joi.string().required().error(new Error('Invalid batch number supplied')),
+  DESCRIPTION: Joi.string().required().error(new Error('Invalid description supplied')),
+  EXPIRY_DATE: Joi.number().required().error(new Error('Invalid expiry date supplied')),
+  ALLERGENS: Joi.object().keys({
+    gluten: Joi.boolean().required().error(new Error('Invalid allergen supplied')),
+    sesameSeeds: Joi.boolean().required().error(new Error('Invalid allergen supplied')),
+    molluscs: Joi.string().required().error(new Error('Invalid allergen supplied')),
+    fish: Joi.boolean().required().error(new Error('Invalid allergen supplied')),
+    soybeans: Joi.boolean().required().error(new Error('Invalid allergen supplied')),
+    peanuts: Joi.boolean().required().error(new Error('Invalid allergen supplied')),
+    milk: Joi.boolean().required().error(new Error('Invalid allergen supplied')),
+    sulphurDioxideAndSulphites: Joi.boolean().required().error(new Error('Invalid allergen supplied')),
+    crustaceans: Joi.boolean().required().error(new Error('Invalid allergen supplied')),
+    eggs: Joi.boolean().required().error(new Error('Invalid allergen supplied')),
+    lupin: Joi.boolean().required().error(new Error('Invalid allergen supplied')),
+    nuts: Joi.boolean().required().error(new Error('Invalid allergen supplied')),
+    mustard: Joi.boolean().required().error(new Error('Invalid allergen supplied')),
+    celery: Joi.boolean().required().error(new Error('Invalid allergen supplied')),
+  }),
+  CREATED_AT: Joi.number().required().error(new Error('Invalid expiry createdAt supplied')),
+  //TO DO - Update for actual schema
+  FROM: Joi.object().keys({
+    company: Joi.string().required().error(new Error('Invalid company supplied')),
+    id: Joi.string().required().error(new Error('Invalid id supplied')),
+    name: Joi.string().required().error(new Error('Invalid name supplied')),
+  }),
+  LIMIT: Joi.number().error(new Error('Invalid limit supplied')),
+  PAGINATED: Joi.boolean().error(new Error('Invalid paginated supplied')),
+  ORDER: Joi.string().error(new Error('Invalid order supplied')),
+};
+
+const hotHolding = {
+  FOOD_ITEM: Joi.number().required().error(new Error('Invalid food item supplied')),
+  TEMPERATURE: Joi.number().required().error(new Error('Invalid temperature supplied')),
+  USER: Joi.string().email().error(new Error('Invalid user supplied')),
+  IMAGE: Joi.string().uri().error(new Error('Invalid image supplied')),
+  COMMENTS: Joi.string().required().error(new Error('Invalid comments supplied')),
+  SIGNATURE: Joi.string().uri().error(new Error('Invalid image supplied')),
+  CREATED_AT: Joi.number().required().error(new Error('Invalid expiry createdAt supplied')),
+  //TO DO - Update for actual schema
+  FROM: Joi.object().keys({
+    company: Joi.string().required().error(new Error('Invalid company supplied')),
+    id: Joi.string().required().error(new Error('Invalid id supplied')),
+    name: Joi.string().required().error(new Error('Invalid name supplied')),
+  }),
+  LIMIT: Joi.number().error(new Error('Invalid limit supplied')),
+  PAGINATED: Joi.boolean().error(new Error('Invalid paginated supplied')),
+  ORDER: Joi.string().error(new Error('Invalid order supplied')),
+};
+
+const fastCooling = {
+  FOOD_ITEM: Joi.number().required().error(new Error('Invalid food item supplied')),
+  TEMPERATURE: Joi.number().required().error(new Error('Invalid temperature supplied')),
+  USER: Joi.string().email().error(new Error('Invalid user supplied')),
+  IMAGE: Joi.string().uri().error(new Error('Invalid image supplied')),
+  COMMENTS: Joi.string().required().error(new Error('Invalid comments supplied')),
+  SIGNATURE: Joi.string().uri().error(new Error('Invalid image supplied')),
+  CREATED_AT: Joi.number().required().error(new Error('Invalid expiry createdAt supplied')),
+  //TO DO - Update for actual schema
+  FROM: Joi.object().keys({
+    company: Joi.string().required().error(new Error('Invalid company supplied')),
+    id: Joi.string().required().error(new Error('Invalid id supplied')),
+    name: Joi.string().required().error(new Error('Invalid name supplied')),
+  }),
+  LIMIT: Joi.number().error(new Error('Invalid limit supplied')),
+  PAGINATED: Joi.boolean().error(new Error('Invalid paginated supplied')),
+  ORDER: Joi.string().error(new Error('Invalid order supplied')),
+};
+
 const schemas = {
   EMAIL: Joi.string().email().required().error(new Error('Invalid email supplied')),
   PASSWORD: Joi.string().required().error(new Error('Invalid password supplied')),
@@ -74,6 +145,9 @@ const schemas = {
   freezer,
   management,
   company,
+  fooditem,
+  hotHolding,
+  fastCooling,
 };
 
 module.exports = schemas;
