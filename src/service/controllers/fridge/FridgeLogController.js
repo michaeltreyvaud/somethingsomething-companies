0dc4b55f430e4b4b58903c9da81cc08894e59701
daniel treyvaud <daniel.treyvaud@gmail.com>
2018-09-18 20:51:07 +0100

    taskAPIs
    Fridge and Freezer Task + Log APIs done

---
+++ src/service/controllers/fridge/FridgeLogController.js
@@ -0,0 +1,201 @@
+const shortid = require('shortid');
+const FridgeLogValidator = require('../../validation/FridgeLogValidator');
+
+class FridgeLogController {
+  constructor(Logger, DocumentClient, CompanyName, TableName) {
+    this.Logger = Logger;
+    this.CompanyName = CompanyName;
+    this.TableName = TableName;
+    this.DocumentClient = DocumentClient;
+    this.Validator = FridgeLogValidator;
+    this.describe = this.describe.bind(this);
+    this.create = this.create.bind(this);
+    this.update = this.update.bind(this);
+    this.delete = this.delete.bind(this);
+    this.list = this.list.bind(this);
+  }
+
+  async describe(req, res, next) {
+    const {
+      Logger, Validator, DocumentClient, TableName, CompanyName,
+    } = this;
+    const { body } = req;
+    Logger.info('describe');
+    try {
+      Validator.validateDescribeRequest(body);
+      const { createdAt } = body;
+      const dbParams = {
+        TableName,
+        Key: { company: CompanyName, createdAt },
+      };
+      const response = await DocumentClient.get(dbParams).promise();
+      return res.status(200).json(response.Item || {});
+    } catch (_err) {
+      return next(_err);
+    }
+  }
+
+  async create(req, res, next) {
+    const {
+      Logger, Validator, DocumentClient,
+      TableName, CompanyName,
+    } = this;
+    const { body } = req;
+    Logger.info('create');
+    try {
+      Validator.validateCreateRequest(body);
+      const {
+        fridgeItem, temperature, user, image, comments, signature,
+      } = body;
+      const date = Date.now();
+      const Item = {
+        company: CompanyName,
+        id: shortid.generate(),
+        fridgeItem,
+        temperature,
+        user,
+        image,
+        comments,
+        signature,
+        createdAt: date,
+        updatedAt: date,
+      };
+      const params = {
+        Item,
+        TableName,
+      };
+      await DocumentClient.put(params).promise();
+      return res.status(200).json(Item);
+    } catch (_err) {
+      return next(_err);
+    }
+  }
+
+  async update(req, res, next) {
+    const {
+      Logger, Validator, DocumentClient,
+      TableName, CompanyName,
+    } = this;
+    const { body } = req;
+    Logger.info('update');
+    try {
+      Validator.validateUpdateRequest(body);
+      const { createdAt } = body;
+      const date = Date.now();
+      let updateExpression = 'set ';
+      const expressionAttributeNames = {
+        '#updatedAt': 'updatedAt',
+        '#company': 'company',
+        '#id': 'id',
+      };
+      const expressionAttributeValues = {
+        ':updatedAt': date,
+        ':company': CompanyName,
+      };
+      updateExpression = `${updateExpression} #updatedAt = :updatedAt`;
+      delete body.id;
+      delete body.company;
+      delete body.createdAt;
+      delete body.updatedAt;
+      Object.keys(body).forEach((key) => {
+        const attr = `#${key}`;
+        const val = `:${key}`;
+        updateExpression = `${updateExpression}, ${attr} = ${val}`;
+        expressionAttributeNames[attr] = key;
+        expressionAttributeValues[val] = body[key];
+      });
+      const dbParams = {
+        TableName,
+        Key: { company: CompanyName, createdAt },
+        UpdateExpression: updateExpression,
+        ExpressionAttributeNames: expressionAttributeNames,
+        ExpressionAttributeValues: expressionAttributeValues,
+        ConditionExpression: 'attribute_exists(#id) AND #company = :company',
+        ReturnValues: 'ALL_NEW',
+      };
+      const item = await DocumentClient.update(dbParams).promise();
+      return res.status(200).json(item.Attributes || {});
+    } catch (_err) {
+      return next(_err);
+    }
+  }
+
+  async delete(req, res, next) {
+    const {
+      Logger, Validator, DocumentClient,
+      TableName, CompanyName,
+    } = this;
+    const { body } = req;
+    Logger.info('delete');
+    try {
+      Validator.validateDeleteRequest(body);
+      const { createdAt } = body;
+      const dbParams = {
+        TableName,
+        Key: { company: CompanyName, createdAt },
+      };
+      await DocumentClient.delete(dbParams).promise();
+      return res.status(200).json({});
+    } catch (_err) {
+      return next(_err);
+    }
+  }
+
+  async list(req, res, next) {
+    const {
+      Logger, Validator, DocumentClient,
+      TableName, CompanyName,
+    } = this;
+    const { body } = req;
+    Logger.info('list');
+    try {
+      Validator.validateListRequest(body);
+      const { from, limit, order } = body;
+      const dbParams = {
+        TableName,
+        // IndexName: 'byName',
+        ExclusiveStartKey: from,
+        Limit: limit || 50,
+        KeyConditionExpression: '#company = :company',
+        ExpressionAttributeNames: {
+          '#company': 'company',
+        },
+        ExpressionAttributeValues: {
+          ':company': CompanyName,
+        },
+        ScanIndexForward: false,
+      };
+      if (order) {
+        const forward = (order === 'asc');
+        dbParams.ScanIndexForward = forward;
+      }
+      const query = async (_params, _options) => {
+        Logger.info('Query');
+        return DocumentClient.query(_params).promise().then((_data) => {
+          if (_options.paginated && _data.LastEvaluatedKey) {
+            const updateParams = {
+              ..._params,
+              ExclusiveStartKey: _data.LastEvaluatedKey,
+            };
+            const newData = _data;
+            return query(updateParams, _options)
+              .then((nextPage) => {
+                newData.Items = newData.Items.concat(nextPage.Items);
+                newData.LastEvaluatedKey = nextPage.LastEvaluatedKey;
+                newData.Count = nextPage.Count;
+                newData.ScannedCount = nextPage.ScannedCount;
+                return newData;
+              });
+          }
+          return _data;
+        });
+      };
+      const result = await query(dbParams, body);
+      return res.status(200).json(result);
+    } catch (_err) {
+      return next(_err);
+    }
+  }
+}
+
+module.exports = FridgeLogController;
---
+++ src/service/validation/FridgeTaskValidator.js
@@ -0,0 +1,54 @@
+const Joi = require('joi');
+const SSError = require('../../util/SSError');
+const schemas = require('./schemas');
+
+const FridgeTaskValidator = {
+  validate(params, schema) {
+    const validationResult = Joi.validate(params, schema);
+    if (validationResult.error) {
+      throw new SSError({ statusCode: 400, message: validationResult.error.message });
+    }
+  },
+  validateDescribeRequest(params) {
+    // const schema = Joi.object().keys({
+    //   createdAt: schemas.fastCooling.CREATED_AT,
+    // });
+    // this.validate(params, schema);
+  },
+  validateCreateRequest(params) {
+    // const schema = Joi.object().keys({
+    //   foodItem: schemas.fastCooling.FOOD_ITEM,
+    //   temperature: schemas.fastCooling.TEMPERATURE,
+    //   user: schemas.fastCooling.USER,
+    //   image: schemas.fastCooling.IMAGE,
+    //   comments: schemas.fastCooling.COMMENTS,
+    //   signature: schemas.fastCooling.SIGNATURE,
+    // });
+    // this.validate(params, schema);
+  },
+  validateUpdateRequest(params) {
+    // const schema = Joi.object().keys({
+    //   id: schemas.fridge.item.ID,
+    //   description: schemas.fridge.item.DESCRIPTION,
+    //   name: schemas.fridge.item.NAME,
+    // });
+    // this.validate(params, schema);
+  },
+  validateDeleteRequest(params) {
+    // const schema = Joi.object().keys({
+    //   createdAt: schemas.fastCooling.CREATED_AT,
+    // });
+    // this.validate(params, schema);
+  },
+  validateListRequest(params) {
+    // const schema = Joi.object().keys({
+    //   limit: schemas.fastCooling.LIMIT,
+    //   from: schemas.fastCooling.FROM,
+    //   paginated: schemas.fastCooling.PAGINATED,
+    //   order: schemas.fastCooling.ORDER,
+    // });
+    // this.validate(params, schema);
+  },
+};
+
+module.exports = FridgeTaskValidator;
